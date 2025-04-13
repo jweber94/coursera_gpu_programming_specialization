2177fbc978f043d2fd25b8329a2d2b39dd371842
Jens Weber <jweber1094@gmail.com>
2025-04-13 11:25:14 +0200

    WIP - understand and implement merge sort on CPU

---
+++ introduction_to_parallel_programming_with_cuda/module2/merge_sort/merge_sort_cpu_jens/main.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+#define DATATYPE unsigned int
+
+std::pair<std::vector<DATATYPE>, std::vector<DATATYPE>> divide(std::vector<DATATYPE>& inputData) {
+    unsigned int divideIdx = std::floor(inputData.size()/2);
+    std::vector<DATATYPE>::iterator divideIterator = inputData.begin();
+    std::advance(divideIterator, divideIdx);
+
+    std::vector<DATATYPE> lhDivide, rhDivide;
+
+    std::copy(inputData.begin(), divideIterator, std::back_inserter(lhDivide));
+    std::copy(divideIterator++, inputData.end(), std::back_inserter(rhDivide));
+    return {lhDivide, rhDivide};
+}
+
+std::vector<DATATYPE> merge(std::vector<DATATYPE>& input1, std::vector<DATATYPE>& input2) {
+    // prepare result
+    std::vector<DATATYPE> ret;
+    ret.reserve(input1.size() + input2.size());
+    
+    // iterate and merge
+    for (auto baseIt = input1.begin(); baseIt != input1.end();) { // no automatic iterator increase since we handle this via the erase() function
+        for (auto runIt = input2.begin(); runIt != input2.end();) { // same here
+            if ((baseIt < runIt)) {
+                ret.push_back(*baseIt);
+                baseIt = input1.erase(baseIt); // needed to avoid iterator invalidation
+            } else if ((runIt < baseIt)) {
+                ret.push_back(*runIt);
+                runIt = input2.erase(runIt);
+            } else if (runIt == input2.end()) {
+                ret.push_back(*runIt);
+                runIt = input2.erase(runIt); // this will be the end element since we were already at the end of the vector
+            } else {
+                std::cerr << "This should not happen" << std::endl;
+            }
+            if (baseIt == input1.end()) {
+                ret.push_back(*baseIt);
+                baseIt = input1.erase(baseIt);
+            }
+        }
+    }
+
+    if (0 != input2.size()) { // if we iterated through input1 but there are open elements in input2, we just append the remaining array to the result since it is already sorted correctly
+        ret.insert(std::end(ret), std::begin(input2), std::end(input2));
+    }
+    return ret;
+}
+
+/**
+ * @brief This should result in a std::vector of single elemented vectors of DATATYPE
+ */
+std::vector<std::vector<DATATYPE>> recursiveDivide(std::vector<DATATYPE> inputToDivide) {
+    auto dividedVecs = divide(inputToDivide);
+    if ((1 == dividedVecs.first.size()) && (1 == dividedVecs.second.size())) {
+        return {dividedVecs.first, dividedVecs.second};
+    } else if (1 != dividedVecs.first.size() && (1 == dividedVecs.second.size())) {
+        auto tmpRet = recursiveDivide(dividedVecs.first);
+        tmpRet.push_back(dividedVecs.second);
+        return tmpRet;
+    } else if (1 == dividedVecs.first.size() && (1 != dividedVecs.second.size())) {
+        auto tmpRet = recursiveDivide(dividedVecs.second);
+        tmpRet.push_back(dividedVecs.first);
+        return tmpRet;
+    } else {
+        auto ret1 = recursiveDivide(dividedVecs.first);
+        auto ret2 = recursiveDivide(dividedVecs.second);
+        ret1.insert(std::end(ret1), std::begin(ret2), std::end(ret2));
+        return ret1;
+    }
+}
+
+std::vector<DATATYPE> recursiveMerge(std::vector<std::vector<DATATYPE>> vecOfDataVecs) {
+    std::vector<DATATYPE> ret;
+    while(1) {
+        std::vector<std::vector<DATATYPE>> tmpRet;
+        std::vector<std::vector<DATATYPE>> tmpBase;
+        unsigned int idx{0};
+        for (unsigned int i = 0; i < tmpBase.size(); i = i + 2) {
+            tmpRet.push_back(merge(vecOfDataVecs.at(i), vecOfDataVecs.at(i+1))); // FIXME: WIP
+        }
+        if (1 == tmpRet.size()) {
+            ret = tmpRet.at(0);
+        }
+    }
+    return ret;
+}
+
+
+std::vector<DATATYPE> mergesort(const std::vector<DATATYPE>& dataToSort) {
+    std::vector<DATATYPE> inputDataToManipulate = dataToSort;
+    
+    auto separatedData = recursiveDivide(inputDataToManipulate); // this is a vector of single elemented vectors
+    
+
+
+    return separatedData.at(0);
+}
+
+void printVec(std::vector<DATATYPE> dataToPrint) {
+    for (auto it = dataToPrint.begin(); it != dataToPrint.end(); it++) {
+        if (std::next(it) != dataToPrint.end()) {
+            std::cout << *it <<", ";
+        } else {
+            std::cout << *it;
+        }
+    }
+    std::cout << std::endl;
+} 
+
+
+/// FIXME: This could be a unit test as well
+int main() {
+    // get data from anywhere
+    std::vector<DATATYPE> dataToSort{5, 2, 4, 8, 1, 9, 7, 3, 6};
+    std::cout << "Data before sort:" << std::endl;
+    printVec(dataToSort);
+    
+    auto sortedData = mergesort(dataToSort);
+
+    std::cout << "Data after sort:" << std::endl;
+    printVec(sortedData);
+    return EXIT_SUCCESS;
+}
